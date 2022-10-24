9f53366c0387c282628f09a81780d4a3dcf6d4be
Martin Kuschnik <martin-kuschnik@LIVE.COM>
2022-10-24 07:58:22 +0200

    added some unit tests

--- src/Native.Net - Tests/IpAddressTests.cpp
+++ src/Native.Net - Tests/IpAddressTests.cpp
@@ -2,6 +2,7 @@
 
 #include "InvalidOperationException.h"
 
+#include "FormatException.h"
 #include "IpAddress.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
@@ -14,6 +15,8 @@ namespace NativeNetTests
 	{
 	public:
 		
+#pragma region Any & IPv6Any
+
 		TEST_METHOD(Any_IsV4Address)
 		{
 			Assert::IsTrue(IpAddress::Any.address_family() == AddressFamily::InterNetwork);
@@ -21,7 +24,7 @@ namespace NativeNetTests
 
 		TEST_METHOD(IPv6Any_IsV6Address)
 		{
-			Assert::IsTrue( IpAddress::IPv6Any.address_family() ==AddressFamily::InterNetworkV6);
+			Assert::IsTrue(IpAddress::IPv6Any.address_family() == AddressFamily::InterNetworkV6);
 		}
 
 
@@ -42,6 +45,10 @@ namespace NativeNetTests
 			for (size_t i = 0; i < 15; i++)
 				Assert::AreEqual(static_cast<UCHAR>(0), addr.u.Byte[i]);
 		}
+
+#pragma endregion
+
+#pragma region Operators
 
 		TEST_METHOD(TypeCastOerator_in6_addr_For_IPv4_ThrowsInvalidOperationException)
 		{
@@ -70,5 +77,54 @@ namespace NativeNetTests
 				// exception successfully thrown
 			}
 		}
+#pragma endregion
+
+#pragma region Parse
+
+		TEST_METHOD(Parse_IpV4Address)
+		{
+			IpAddress ipv4 = IpAddress::Parse("192.168.178.1");
+
+			Assert::IsTrue(ipv4.address_family() == AddressFamily::InterNetwork);
+		}
+
+		TEST_METHOD(Parse_ThrowsFormatExceptionForInvalidIpv4)
+		{
+			try
+			{
+				IpAddress::Parse("192.168.178");
+
+				Assert::Fail(L"IpAddress::Parse(..) did not throw.");
+			}
+			catch (const FormatException&)
+			{
+				// exception successfully thrown
+			}
+		}
+
+		TEST_METHOD(Parse_IpV6Address)
+		{
+			IpAddress ipv6 = IpAddress::Parse("2001:0db8:85a3:0000:0000:8a2e:0370:7334");
+
+			Assert::IsTrue(ipv6.address_family() == AddressFamily::InterNetworkV6);
+		}
+
+		TEST_METHOD(Parse_ThrowsFormatExceptionForInvalidIpv6)
+		{
+			try
+			{
+				IpAddress::Parse("2001:0db8:85a3:0000:0000:8a2e:0370:");
+
+				Assert::Fail(L"IpAddress::Parse(..) did not throw.");
+			}
+			catch (const FormatException&)
+			{
+				// exception successfully thrown
+			}
+		}
+
+#pragma endregion
+
+
 	};
 }
