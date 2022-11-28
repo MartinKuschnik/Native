cc0ca3e0096e25c37e3224139857a7390da69b12
Martin Kuschnik <martin-kuschnik@LIVE.COM>
2022-11-28 08:27:50 +0100

    add test project for ServiceModel project

---
+++ src/Native.ServiceModel-Tests/Native.ServiceModel-Tests.cpp
@@ -0,0 +1,15 @@
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace NativeServiceModelTests
+{
+	TEST_CLASS(NativeServiceModelTests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
