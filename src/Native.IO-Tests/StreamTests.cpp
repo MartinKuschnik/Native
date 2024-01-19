95304e8c8451ea4fd32522dde6ed0a9a81dad56e
Martin Kuschnik <martin-kuschnik@LIVE.COM>
2024-01-19 10:32:55 +0100

    added tests for Stream::copy_to

---
+++ src/Native.IO-Tests/StreamTests.cpp
@@ -0,0 +1,56 @@
+#include "CppUnitTest.h"
+
+#include <array>
+
+#include "Stream.h"
+#include "MemoryStream.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+using namespace Native;
+using namespace Native::IO;
+
+namespace NativeIOTests
+{
+	TEST_CLASS(StreamTests)
+	{
+	public:
+
+		TEST_METHOD(copy_to_copies_all_data_with_one_third_of_buffer_size)
+		{
+			// Arrange
+			const std::string input_string = "This is the data that is written and read to/from the MemoryStream.";
+
+			MemoryStream mem_stream_1(input_string.size());
+			MemoryStream mem_stream_2(input_string.size());
+
+			mem_stream_1.write(input_string.data(), input_string.size());
+			mem_stream_1.seek(0, SeekOrigin::Begin);
+
+			// Act
+			const size_t copied = mem_stream_1.copy_to(&mem_stream_2, input_string.length() / 3);
+
+			// Assert
+			Assert::AreEqual(std::string_view(input_string), std::string_view(reinterpret_cast<const char*>(mem_stream_2.data()), copied));
+		}
+
+		TEST_METHOD(copy_to_copies_all_data_with_full_buffer_size)
+		{
+			// Arrange
+			const std::string input_string = "This is the data that is written and read to/from the MemoryStream.";
+
+			MemoryStream mem_stream_1(input_string.size());
+			MemoryStream mem_stream_2(input_string.size());
+
+			mem_stream_1.write(input_string.data(), input_string.size());
+			mem_stream_1.seek(0, SeekOrigin::Begin);
+
+			// Act
+			const size_t copied = mem_stream_1.copy_to(&mem_stream_2, input_string.length());
+
+			// Assert
+			Assert::AreEqual(std::string_view(input_string), std::string_view(reinterpret_cast<const char*>(mem_stream_2.data()), copied));
+		}
+
+	};
+}
