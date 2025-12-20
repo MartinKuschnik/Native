0c1cb33bcc1b8460cc52ee6cd6e9ff95b56c383c
Martin Kuschnik <Martin-Kuschnik@live.com>
2025-12-20 10:33:36 +0100

    blub

--- src/Benchmarking/GuidBenchmarks.cpp
+++ src/Benchmarking/GuidBenchmarks.cpp
@@ -33,6 +33,8 @@ static void Guid_ParseWithBracketsW(benchmark::State& state) {
 	{
 		const Native::Guid guid = Native::Guid::Parse(L"{122AACBB-8F4B-486C-8C13-9330A0679126}");
 
+		std::this_thread::sleep_for(std::chrono::seconds(1));
+
 		benchmark::DoNotOptimize(guid);
 	}
 }
