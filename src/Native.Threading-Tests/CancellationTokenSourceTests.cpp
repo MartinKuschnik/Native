477556360dc6ba68f3019eb6553efface063898b
Martin Kuschnik <Martin-Kuschnik@live.com>
2025-12-20 09:55:55 +0100

    Add messages to assertions in CancellationTokenSourceTests

--- src/Native.Threading-Tests/CancellationTokenSourceTests.cpp
+++ src/Native.Threading-Tests/CancellationTokenSourceTests.cpp
@@ -24,11 +24,11 @@ namespace NativeThreadingTests
 			CancellationTokenSource cancellationTokenSource;
 			const CancellationToken token = cancellationTokenSource.Token;
 
-			Assert::IsFalse(token.is_cancellation_requested());
+			Assert::IsFalse(token.is_cancellation_requested(), L"Token should not be canceled before cancel() is called");
 
 			cancellationTokenSource.cancel();
 
-			Assert::IsTrue(token.is_cancellation_requested());
+			Assert::IsTrue(token.is_cancellation_requested(), L"Token should be canceled after cancel() is called");
 		}
 
 		TEST_METHOD(token_with_delay_can_be_canceled_earlier)
@@ -36,11 +36,11 @@ namespace NativeThreadingTests
 			CancellationTokenSource cancellationTokenSource(1h);
 			const CancellationToken token = cancellationTokenSource.Token;
 
-			Assert::IsFalse(token.is_cancellation_requested());
+			Assert::IsFalse(token.is_cancellation_requested(), L"Token with delay should not be canceled initially");
 
 			cancellationTokenSource.cancel();
 
-			Assert::IsTrue(token.is_cancellation_requested());
+			Assert::IsTrue(token.is_cancellation_requested(), L"Token with delay should be canceled after explicit cancel() call");
 		}
 
 		TEST_METHOD(token_with_delay_gets_canceled_after_delay)
@@ -48,13 +48,13 @@ namespace NativeThreadingTests
 			const CancellationTokenSource cancellationTokenSource(100ms);
 			const CancellationToken token = cancellationTokenSource.Token;
 
-			Assert::IsFalse(token.is_cancellation_requested());
+			Assert::IsFalse(token.is_cancellation_requested(), L"Token should not be canceled immediately after creation");
 			std::this_thread::sleep_for(50ms);
-			Assert::IsFalse(token.is_cancellation_requested());
+			Assert::IsFalse(token.is_cancellation_requested(), L"Token should not be canceled after 50ms (delay is 100ms)");
 
 			std::this_thread::sleep_for(50ms);
 
-			Assert::IsTrue(token.is_cancellation_requested());
+			Assert::IsTrue(token.is_cancellation_requested(), L"Token should be canceled after 100ms delay has elapsed");
 		}
 
 		TEST_METHOD(token_with_due_time_can_be_canceled_earlier)
@@ -64,11 +64,11 @@ namespace NativeThreadingTests
 			CancellationTokenSource cancellationTokenSource(due_time);
 			const CancellationToken token = cancellationTokenSource.Token;
 
-			Assert::IsFalse(token.is_cancellation_requested());
+			Assert::IsFalse(token.is_cancellation_requested(), L"Token with due time should not be canceled initially");
 
 			cancellationTokenSource.cancel();
 
-			Assert::IsTrue(token.is_cancellation_requested());
+			Assert::IsTrue(token.is_cancellation_requested(), L"Token with due time should be canceled after explicit cancel() call");
 		}
 
 		TEST_METHOD(token_with_due_time_gets_canceled_at_due_time)
@@ -78,13 +78,13 @@ namespace NativeThreadingTests
 			const CancellationTokenSource cancellationTokenSource(due_time);
 			const CancellationToken token = cancellationTokenSource.Token;
 
-			Assert::IsFalse(token.is_cancellation_requested());
+			Assert::IsFalse(token.is_cancellation_requested(), L"Token should not be canceled immediately after creation");
 			std::this_thread::sleep_for(50ms);
-			Assert::IsFalse(token.is_cancellation_requested());
+			Assert::IsFalse(token.is_cancellation_requested(), L"Token should not be canceled after 50ms (due time is 100ms)");
 
 			std::this_thread::sleep_for(50ms);
 
-			Assert::IsTrue(token.is_cancellation_requested());
+			Assert::IsTrue(token.is_cancellation_requested(), L"Token should be canceled after due time has been reached");
 		}
 	};
 }
